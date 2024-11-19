fb04dea0483d825f1440cf53a2fa91ad34078b49
Lucas <cheroyonatan2@gmail.com>
2024-11-19 18:03:40 -0300

    Agrego hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
 import '../styles/Hero.scss'
+import heroPhoto from '../assets/images/hero-photo.svg'
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='hero'>
+      <div className='hero-text'>
+        <h1 className='hero-title'>Navigating the digital landscape for success</h1>
+        <p>
+          Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.
+        </p>
+        <button>Book a consultation</button>
+      </div>
+      <div className='hero-image'>
+        <img src={heroPhoto} alt="Foto Hero" />
+      </div>
     </div>
   )
 }
