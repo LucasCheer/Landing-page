7c9dbfdf55db8cd12ddc4ffaaaebc7885b41b42a
Lucas <cheroyonatan2@gmail.com>
2024-11-23 20:53:51 -0300

    Agrego service-section

--- src/sections/ServicesSection.jsx
+++ src/sections/ServicesSection.jsx
@@ -3,12 +3,16 @@ import Card from '../components/Card.jsx'
 
 const ServicesSection = () => {
   return (
-    <section>
+    <section className='service-section'>
       <div className='title-container'>
         <h4>Services</h4>
         <p>At our digital marketing agency, we offer a range of services to help businesses grow and succeed online. These services include:</p>
       </div>
       <div className='card-container'>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
         <Card/>
         <Card/>
       </div>
