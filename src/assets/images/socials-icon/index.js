a2028e90f92e1fc6888641760e04a32b6ab95150
Lucas <cheroyonatan2@gmail.com>
2024-11-26 15:38:07 -0300

    Agrego footer

---
+++ src/assets/images/socials-icon/index.js
@@ -0,0 +1,5 @@
+import iconLinkedin from './linkedin-icon.svg'
+import iconX from './x-icon.svg'
+import iconFace from './face-icon.svg'
+
+export { iconLinkedin, iconX, iconFace }
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,9 +1,49 @@
 import '../styles/sections/Footer.scss'
+import logoDark from '../../public/logo-dark.svg'
+import { iconFace, iconLinkedin, iconX } from '../assets/images/socials-icon'
+import Button from '../components/Button'
 
 const Footer = () => {
     return (
-        <footer>
-            <h1></h1>
+        <footer className='footer'>
+            <div className='footer-container'>
+                <div className='sup-container'>
+                    <nav className='footer-nav'>
+                        <img src={logoDark} alt="Logo" />
+                        <ul>
+                            <li>About us</li>
+                            <li>Services</li>
+                            <li>Use Cases</li>
+                            <li>Pricing</li>
+                            <li>Blog</li>
+                        </ul>
+                        <div className='footer-social'>
+                            <img src={iconLinkedin} alt="Icon Social" />
+                            <img src={iconFace} alt="Icon Social" />
+                            <img src={iconX} alt="Icon Social" />
+                        </div>
+                    </nav>
+                    <div className='footer-contact'>
+                        <div className='contact-info'>
+                            <h6>Contact us</h6>
+                            <div className='p-container'>
+                                <p>Email: info@positivus.com</p>
+                                <p>Phone: 555-567-8901</p>
+                                <p>Address: 1234 Main St
+                                Moonstone City, Stardust State 12345</p>
+                            </div>
+                        </div>
+                        <form className='contact-form' action="">
+                            <input type="text" placeholder='Email' />
+                            <Button text='Subscribe to news' color='tertiary' />
+                        </form>
+                    </div>
+                </div>
+                <hr />
+                <div className='inf-container'>
+                    <p>© 2024. Positivus. All rights reserved.</p>
+                </div>
+            </div>
         </footer>
     )
 }
