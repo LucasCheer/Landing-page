7047b9667b813ecd568e8bc9be75160ac6c4fa75
Lucas <cheroyonatan2@gmail.com>
2024-11-27 21:03:55 -0300

    Cambio de styles en Card

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import '../styles/components/Navbar.scss'
 import Button from './Button'
+import logo from '../../public/logo.svg'
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
       <div className='logo-container'>
-        <img src="../../public/logo.svg" alt="logo" />
-        <img src="../../public/Positivus.svg" alt="" />
+        <img src={logo} alt="logo" />
       </div>
       <ul className='list'>
         <li className="item">About us</li>
